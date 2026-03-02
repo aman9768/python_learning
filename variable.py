72261a79a8535ee5aa564e841b11f18e4846b5d0
aman9768 <aman983322@gmail.com>
2026-03-02 16:31:25 +0530

    My first push

---
+++ variable.py
@@ -0,0 +1,8 @@
+from loguru import logger
+
+shop_name ="Big Bites"
+product_name_1="waffles"
+product_name_2="pancakes"
+product_name_3="cold coffee"
+
+logger.info(f"The name of shop is {shop_name} and there product like {product_name_1}, {product_name_2} and {product_name_3} and much more.")   
